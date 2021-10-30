01ea735ebd6321d321a76613e16de1b8dcff09b8
Gaby <gabybabuchi@gmail.com>
2021-10-30 04:03:56 -0400

    Create base structure of characters.
    Define a struct to save
    character properties.
    Set the constructor to
    receive certain properties and
    initialize default characters.

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,39 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("First NFT game on Ethereum! Wu huu!");
+
+    //struct to save character properties
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint ar;//audience rating (HP)
+        uint maxAr;//(max HP)
+        uint charismaP;//a serial with higer amount of charisma points will decrese with a faster rate the oponents audience(attackDamage)
+    }
+
+    //array of default characters
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterAr,
+        uint[] memory characterCharismaP
+        ) 
+        {
+            for( uint i = 0; i < characterNames.length; i+=1){
+                defaultCharacters.push(CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    ar: characterAr[i],
+                    maxAr: characterAr[i],
+                    charismaP: characterCharismaP[i]
+                }));
+
+                CharacterAttributes memory c = defaultCharacters[i];
+                console.log("Done initializing %s w/ an audience rating of %s, img %s",c.name, c.ar, c.imageURI);
+            }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,14 @@ const main = async () => {
     //compile contract and generate files under artifact directory
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     //create a local Ethereum network for this project via hardhat
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Modern Family", "HIMYM", "Brooklyng 99"], //Names
+        ["https://i.imgur.com/pKd5Sdk.png", // Images
+            "https://i.imgur.com/xVu4vFL.png",
+            "https://i.imgur.com/WMB6g9u.png"],
+        [500, 400, 300], //HP values
+        [200, 150, 300] //Attack damage values
+    );
     //await for the contract to be mined by the network fake miners
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
